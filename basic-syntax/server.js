6d4ab0493b426a273e7137e969f5474dc0be395f
Merc5Dark <31131794siya@gmail.com>
2023-11-30 12:28:17 +0200

    Express Server

--- basic-syntax/server.js
+++ basic-syntax/server.js
@@ -1,15 +1,18 @@
-// Load HTTP module
-let http = require("http");
+// // Load HTTP module
+// let http = require("http");
 
-// Create HTTP server and listen on port 8080 for requests 
-http.createServer(function (request, response) {
+// // Create HTTP server and listen on port 8080 for requests 
+// http.createServer(function (request, response) {
 
-    // Set the response HTTP header with HTTP status and Content type
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+//     // Set the response HTTP header with HTTP status and Content type
+//     response.writeHead(200, {'Content-Type': 'text/plain'});
     
-    // Send response body
-    response.end('Hello Node\n');
-}).listen(8080);
+//     // Send response body
+//     response.end('Hello Node\n');
+// }).listen(8080);
 
-// Print URL for accessing server
-console.log('Server running at http://127.0.0.1:8080/');
+// // Print URL for accessing server
+// console.log('Server running at http://127.0.0.1:8080/');
+
+////////////////////////////////////////////////////////////////////////
+
