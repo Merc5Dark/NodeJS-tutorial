1d048d8ed393babcb493ac2d38997f555646d545
Merc5Dark <31131794siya@gmail.com>
2023-11-30 13:56:50 +0200

    expreess

--- basic-syntax/server.js
+++ basic-syntax/server.js
@@ -1,30 +1,29 @@
 // // Load HTTP module
-// let http = require("http");
+let http = require("http");
 
-// // Create HTTP server and listen on port 8080 for requests 
-// http.createServer(function (request, response) {
+// Create HTTP server and listen on port 8080 for requests 
+http.createServer(function (request, response) {
 
-//     // Set the response HTTP header with HTTP status and Content type
-//     response.writeHead(200, {'Content-Type': 'text/plain'});
+    // Set the response HTTP header with HTTP status and Content type
+    response.writeHead(200, {'Content-Type': 'text/plain'});
     
-//     // Send response body
-//     response.end('Hello Node\n');
-// }).listen(8080);
+    // Send response body
+    response.end('Hello Node\n');
+}).listen(8080);
 
-// // Print URL for accessing server
-// console.log('Server running at http://127.0.0.1:8080/');
+// Print URL for accessing server
+console.log('Server running at http://127.0.0.1:8080/');
 
 ////////////////////////////////////////////////////////////////////////
 
 
-// let express = require('express');
-// let app = express();
+let express = require('express');
+let app = express();
 
-// app.get('/', function (req, res) {
-//     res.send('Hello Node');
-// });
+app.get('/', function (req, res) {
+    res.send('Hello Node');
+});
 
-// app.listen(8080, function () {
-//     console.log('Example app listening on port 8080!');
-// });
-
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!');
+})
